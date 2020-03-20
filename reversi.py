47b0b53ab7129b4829c54a72d93c623da7715f1f
Silke Meiner <s81320@beuth-hochschule.de>
2020-03-20 23:47:26 +0100

    changes in data type of Board.board from dict to numpy array, error: set stone at boundary and get rejection

--- reversi.py
+++ reversi.py
@@ -1,15 +1,18 @@
-# March 2nd 2020
+# March 20th 2020
+# change data structure of board from dict to numpy array
+# change player numbers / player ids from 0 and 1 to -1 and 1
+# change empty spots on board from -1 to 0
+# new error: stone set at boundary is not accepted
 """Board game reversi (aka othello) for 2 players. Game is played at one terminal."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 #run with python or pythonw (for MacOS)
 
-
 def input_stone_position():
     
-    """Asks a player to give two integers as coordinates where to put his/her stone.
-    returns a position as a tuple. No error handling."""
+    """Asks a player to give two integers as coordinates where to put their stone.
+    returns a position as a tuple."""
     p_1 = input("input first co-ordinate, range 0 to 7:")
     p_2 = input("input second co-ordinate, range 0 to 7:")
     
@@ -28,13 +31,22 @@ def input_stone_position():
 
 def opponent(i):
     """Calculate the id for the other player, the opponent."""
-    return (1+i)%2
+    return -i
+
+def player_sequence(i):
+    return int((1+i)/2)
+
+def add(x,y): return x+y
+
+class Position(tuple):
+    def step(self,direction):
+        return Position(map(add,self,direction))
 
 class Player:
 
     """Two players compete against each other.
     Players communicate through their mediator, the Host.
-    Players are identified by their (number), either 0 or 1."""
+    Players are identified by their (number), either -1 or 1."""
     def __init__(self):
         """initialize. Set the number. increase the number of created players."""
         self.number: int
@@ -42,7 +54,7 @@ class Player:
     def propose_stone(self):
         """A player-object gets a keyboard-input from the player-human. If not successful (out of bounds, not free), no stone is set and the game continues (with the next player)."""
         colours = ['red', 'blue']
-        print("Set a "+colours[self.number]+" stone.")
+        print("Set a "+colours[player_sequence(self.number)]+" stone.")
         return input_stone_position()
 
 
@@ -50,7 +62,7 @@ class Draw:
     def __init__(self):
         """Setting the basic information, starting empty."""
         self.player: int = 0
-        self.position: tuple = ()
+        self.position: Position
         self.accepted = True
         self.directions_enclosing: list = []
         self.final = False
@@ -75,14 +87,14 @@ class Board:
         """Init."""
         self.size = 8
         self.max_nr_stones = 64
-        self.board = {(k, l):-1 for k in range(self.size) for l in range(self.size)}
+        self.board = np.zeros(shape=(self.size,self.size),dtype=int)
         self.score = [] # empty list
         self.stones_set = 0
 
     def setup(self):
         """Initially there are 4 stones at the center of the board, two in each players colour."""
-        self.board[(3, 3)] = 0
-        self.board[(3, 4)] = 0
+        self.board[(3, 3)] = -1
+        self.board[(3, 4)] = -1
         self.board[(4, 3)] = 1
         self.board[(4, 4)] = 1
 
@@ -91,9 +103,11 @@ class Board:
     def update_scores(self):
         """To be called after a new stone has been set. Calculates from scratch."""
         self.score = [0, 0]
-        stones_on_board = list(self.board.values())
-        for i in range(2):
-            self.score[i] = sum(1 for j in range(len(stones_on_board)) if stones_on_board[j] == i)
+        #stones_on_board = list(self.board.values())
+        
+        self.score[0] = (-1)*sum(self.board[self.board==-1])
+        self.score[1] = sum(self.board[self.board==1])
+            #self.score[i] = sum(1 for j in range(len(stones_on_board)) if stones_on_board[j] == i)
 
     def get_scores(self):
         """Simple getter function. Returns the score of both players. Red player's score first."""
@@ -106,10 +120,11 @@ class Board:
     def update(self, draw):
         """When a player has put a new stone on the board newly includes / cought stones turn change color."""
         for direction in draw.directions_enclosing:
-            pos = tuple(draw.position)
+            pos = draw.position
             done = False
             while not done:
-                next_stone_in_line = tuple(x+y for x, y in zip(pos, direction))
+                next_stone_in_line = pos.step(direction)
+                #next_stone_in_line = tuple(x+y for x, y in zip(pos, direction))
                 if self.board[next_stone_in_line] == opponent(draw.player):
                     self.board[next_stone_in_line] = draw.player
                     pos = next_stone_in_line
@@ -119,8 +134,8 @@ class Board:
     def print(self):
         """Outputs the board as a graph."""
         # it would be nice just to add one point instead of printing all again from scratch
-        stones_player_0 = [k for k, v in self.board.items() if v == 0]
-        stones_player_1 = [k for k, v in self.board.items() if v == 1]
+        stones_player_0 = [(i,j) for i in range(self.size) for j in range(self.size) if self.board[i,j]==-1]
+        stones_player_1 = [(i,j) for i in range(self.size) for j in range(self.size) if self.board[i,j]==1]
         plt.plot([0, self.size-1, 0, self.size-1], [0, 0, self.size-1, self.size-1], marker='x', ls='')
         plt.plot(*zip(*stones_player_0), marker='o', color='r', ls='')
         plt.plot(*zip(*stones_player_1), marker='o', color='b', ls='')
@@ -130,7 +145,7 @@ class Board:
 
     def put_stone_on_board(self, draw):
         """From the (draw) the player number is applied to set the value on the boards board."""
-        self.board[tuple(draw.position)] = draw.player
+        self.board[draw.position] = draw.player
         self.stones_set += 1
 
 class RuleChecker:
@@ -164,7 +179,7 @@ class RuleChecker:
         """Check if a (position) is free. Returns True iff no stone is already placed on that position."""
         if pos is None:
             pos = self.draw.position
-        return self.board.board[pos] == -1
+        return self.board.board[pos] == 0
 
     def check_position_for_same_occupancy(self, position1, position2):
         """Returns True if on both positions there is the same colour or if both positions are empty."""
@@ -205,7 +220,7 @@ class RuleChecker:
         all_directions = ((1, 0), (-1, 0), (0, 1), (0, -1), (-1, -1), (-1, 1), (1, 1), (1, -1))
 
         # filter returns an iterator, we have to materialize the filter and turn it into a tuple (or list or whatever...)
-        return tuple(filter(lambda x: self.check_position_exists(tuple(np.array(pos)+x)), all_directions))
+        return tuple(filter(lambda x: self.check_position_exists(pos.step(x)), all_directions))
         # all directions used to be a list, now it is a tuple. Seems to work fine...
         # filter needs the data to be filtered as a iterable container
 
@@ -213,8 +228,7 @@ class RuleChecker:
 
         """Filters directions such that (draw.position) + (direction) is of different colour than the players own stone (draw.player).
         Requires input (directions) to be of ingoing directions only."""
-        return tuple(filter(lambda x: self.check_for_different_colour(tuple(np.array(self.draw.position)+np.array(x)), self.draw.player), directions))
-
+        return tuple(filter(lambda x: self.check_for_different_colour(self.draw.position.step(x), self.draw.player), directions))
 
     def walk_on_beam(self, colour, position, direction):
 
@@ -227,7 +241,7 @@ class RuleChecker:
         Walking on that beam we take one step at a time, only to the (next_pos).
         We return False if the beam of opponents stone ends in an empty field or at the boundary of the board.
         and True if we meet a stone of our initially set (colour)."""
-        next_pos = tuple(np.array(position) + np.array(direction))
+        next_pos = position.step(direction)
         if not self.check_position_exists(next_pos): # position is on the boundary, next_pos is off the board
             return False
         elif self.check_position_free(next_pos): # position exists, is on board -> but empty
@@ -243,7 +257,7 @@ class RuleChecker:
         dir_touch_opponent = self.select_directions_touching(directions)
         start_pos = self.draw.position
         player_id = self.draw.player
-        return list(filter(lambda d: self.walk_on_beam(player_id, np.array(start_pos) + np.array(d), np.array(d)), dir_touch_opponent))
+        return list(filter(lambda d: self.walk_on_beam(player_id, start_pos.step(d), d), dir_touch_opponent))
 
     def game_on(self):
 
@@ -280,7 +294,7 @@ def main():
     board.print_scores()
 
     game_on = True
-    player.number = 0
+    player.number = -1
 
     draw = Draw()
     documentation = [Draw()] # initialize with one draw. This draw has accepted = True.
@@ -288,7 +302,7 @@ def main():
     while game_on:
         draw = Draw()
         draw.player = player.number
-        draw.position = player.propose_stone()
+        draw.position = Position(player.propose_stone())
         rule_checker = RuleChecker(board, draw, documentation)
         draw.directions_enclosing, draw.accepted = rule_checker.check_position()
 
